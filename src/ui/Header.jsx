9944520a1038c59e0097939544cb761e9dbff5d5
Glenn Pinto <gvpinto@gmail.com>
2023-09-02 08:29:07 -0400

    286 Building the AppLayout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Fast React Pizze Co.</Link>
+        </header>
+    );
+}
+
+export default Header;
