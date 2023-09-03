8f1dbf5480e93b02b98f6dfa807f4be0f52056da
Glenn Pinto <gvpinto@gmail.com>
2023-09-03 16:27:28 -0400

    291. Writing data with react router Actions

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Cart from './features/cart/Cart';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from "./ui/AppLayout";
 
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
             { path: '/', element: <Home /> },
             { path: '/cart', element: <Cart /> },
             { path: '/menu', element: <Menu />, loader: menuLoader, errorElement: <Error />, },
-            { path: '/order/new', element: <CreateOrder /> },
+            { path: '/order/new', element: <CreateOrder />, action: createOrderAction },
             { path: '/order/:orderId', element: <Order />, loader: orderLoader, errorElement: <Error />, },
         ]
     }
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,7 +40,8 @@ function CreateOrder() {
         <div>
             <h2>Ready to order? Let&aposs go!</h2>
 
-            <form>
+            {/* <Form method="POST" action="/order/new"> */}
+            <Form method="POST">
                 <div>
                     <label>First Name</label>
                     <input type="text" name="customer" required />
@@ -70,11 +73,28 @@ function CreateOrder() {
                 </div>
 
                 <div>
+                    <input type="hidden" value={JSON.stringify(cart)} name="cart" />
                     <button>Order now</button>
                 </div>
-            </form>
+            </Form>
         </div>
     );
+}
+
+
+export async function action({ request }) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    const order = {
+        ...data,
+        cart: JSON.parse(data.cart),
+        priority: data.priority === 'on',
+    };
+    // console.log(order);
+    const newOrder = await createOrder(order);
+    // console.log(newOrder);
+    return redirect(`/order/${newOrder.id}`);
+    // return null;
 }
 
 export default CreateOrder;
