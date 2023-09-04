3642a76b885d8db1ef368145ce4dce1b46170dec
Glenn Pinto <gvpinto@gmail.com>
2023-09-03 23:36:16 -0400

    295/296. Setting update tailwind and working with Color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/searchOrder";
 
 function Header() {
-    return (
-        <header>
-            <Link to="/">Fast React Pizze Co.</Link>
-            <SearchOrder />
-        </header>
-    );
+  return (
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React Pizze Co.</Link>
+      <SearchOrder />
+      Jonas
+    </header>
+  );
 }
 
 export default Header;
