13009bf2fbdaf6ca778eb54809144c6f47756a5f
Glenn Pinto <gvpinto@gmail.com>
2023-09-05 18:23:38 -0400

    301. Using CSS Grid

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,7 @@
 function Username() {
-  return <div className="hidden text-xs font-semibold">Glenn Pinto</div>;
+  return (
+    <div className="hidden text-xs font-semibold md:block">Glenn Pinto</div>
+  );
 }
 
 export default Username;
