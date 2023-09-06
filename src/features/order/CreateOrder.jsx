2adb7abf227953617f213f7b998ead5b7230da0c
Glenn Pinto <gvpinto@gmail.com>
2023-09-05 20:22:39 -0400

    302. Styling Buttons: Element States and Transitions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,113 +4,134 @@ import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
-    /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-        str
-    );
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
 
 const fakeCart = [
-    {
-        pizzaId: 12,
-        name: "Mediterranean",
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: "Vegetale",
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: "Spinach and Mushroom",
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
+  // const isErrors = !!errors;
 
-    const navigation = useNavigation();
-    const isSubmitting = navigation.state === 'submitting';
-    const formErrors = useActionData();
-    // const isErrors = !!errors;
+  // const [withPriority, setWithPriority] = useState(false);
+  const cart = fakeCart;
 
-    // const [withPriority, setWithPriority] = useState(false);
-    const cart = fakeCart;
+  return (
+    <div>
+      <h2>Ready to order? Let&aposs go!</h2>
 
-    return (
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
         <div>
-            <h2>Ready to order? Let&aposs go!</h2>
-
-            {/* <Form method="POST" action="/order/new"> */}
-            <Form method="POST">
-                <div>
-                    <label>First Name</label>
-                    <input type="text" name="customer" required />
-                </div>
-
-                <div>
-                    <label>Phone number</label>
-                    <div>
-                        <input type="tel" name="phone" required />
-                        {formErrors?.phone && <p>{formErrors.phone}</p>}
-                    </div>
-                </div>
-
-                <div>
-                    <label>Address</label>
-                    <div>
-                        <input type="text" name="address" required />
-                    </div>
-                </div>
-
-                <div>
-                    <input
-                        type="checkbox"
-                        name="priority"
-                        id="priority"
-                    // value={withPriority}
-                    // onChange={(e) => setWithPriority(e.target.checked)}
-                    />
-                    <label htmlFor="priority">Want to yo give your order priority?</label>
-                </div>
-
-                <div>
-                    <input type="hidden" value={JSON.stringify(cart)} name="cart" />
-                    <button disabled={isSubmitting}>{isSubmitting ? 'Creating order...' : 'Order now'}</button>
-                </div>
-            </Form>
+          <label>First Name</label>
+          <input type="text" name="customer" required />
         </div>
-    );
+
+        <div>
+          <label>Phone number</label>
+          <div>
+            <input type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
+          </div>
+        </div>
+
+        <div>
+          <label>Address</label>
+          <div>
+            <input type="text" name="address" required />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority">Want to yo give your order priority?</label>
+        </div>
+
+        <div>
+          <input type="hidden" value={JSON.stringify(cart)} name="cart" />
+          <button
+            disabled={isSubmitting}
+            className="inline-block 
+            rounded-full 
+            bg-yellow-400 
+            px-4 py-3 
+            font-semibold 
+            uppercase 
+            tracking-wide
+            text-stone-800 
+            transition-colors 
+            duration-300 
+            hover:bg-yellow-300
+            focus:bg-yellow-300
+            focus:outline-none
+            focus:ring
+            focus:ring-yellow-300
+            focus:ring-offset-2
+            active:bg-yellow-400
+            disabled:cursor-not-allowed
+            disabled:bg-slate-300"
+          >
+            {isSubmitting ? "Creating order..." : "Order now"}
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
-
 export async function action({ request }) {
-    const formData = await request.formData();
-    const data = Object.fromEntries(formData);
-    const order = {
-        ...data,
-        cart: JSON.parse(data.cart),
-        priority: data.priority === 'on',
-    };
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
 
-    const errors = {};
+  const errors = {};
 
-    if (!isValidPhone(order.phone)) {
-        errors.phone = "Please enter a valid phone number";
-    }
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Please enter a valid phone number";
+  }
 
-    if (Object.keys(errors).length > 0) return errors;
+  if (Object.keys(errors).length > 0) return errors;
 
-    // console.log(order);
-    const newOrder = await createOrder(order);
-    console.log(newOrder.id);
-    return redirect(`/order/${newOrder.id}`);
-    // return null;
+  // console.log(order);
+  //   const newOrder = await createOrder(order);
+  //   console.log(newOrder.id);
+  //   return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 
 export default CreateOrder;
