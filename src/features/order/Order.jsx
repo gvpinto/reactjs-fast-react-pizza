2d30ca10e99f4e948553d77a0d987974c2405716
Glenn Pinto <gvpinto@gmail.com>
2023-09-08 15:35:03 -0400

    310. Styling Order Form, 311. Styling Orderview

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -4,10 +4,11 @@
 import { useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
-    calcMinutesLeft,
-    formatCurrency,
-    formatDate,
+  calcMinutesLeft,
+  formatCurrency,
+  formatDate,
 } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 /*
 const order = {
@@ -47,52 +48,65 @@ const order = {
 */
 
 function Order() {
+  const order = useLoaderData();
+  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+    cart,
+  } = order;
+  const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
-    const order = useLoaderData();
-    // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-    const {
-        id,
-        status,
-        priority,
-        priorityPrice,
-        orderPrice,
-        estimatedDelivery,
-        cart,
-    } = order;
-    const deliveryIn = calcMinutesLeft(estimatedDelivery);
+  return (
+    <div className="space-y-8 px-4 py-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order# {id} status</h2>
 
-    return (
-        <div>
-            <div>
-                <h2>Status</h2>
-
-                <div>
-                    {priority && <span>Priority</span>}
-                    <span>{status} order</span>
-                </div>
-            </div>
-
-            <div>
-                <p>
-                    {deliveryIn >= 0
-                        ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-                        : "Order should have arrived"}
-                </p>
-                <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
-            </div>
-
-            <div>
-                <p>Price pizza: {formatCurrency(orderPrice)}</p>
-                {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-                <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
-            </div>
+        <div className="space-x-2">
+          {priority && (
+            <span className="rounded-full bg-red-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-red-50">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-50">
+            {status} order
+          </span>
         </div>
-    );
+      </div>
+
+      <div className="flex flex-wrap items-center justify-between  gap-2 bg-stone-200 px-6 py-5">
+        <p className="font-medium">
+          {deliveryIn >= 0
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            : "Order should have arrived"}
+        </p>
+        <p className="text-sm text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
+      </div>
+
+      <ul className="divide-y divide-gray-200 border-b border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
+
+      <div>
+        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      </div>
+    </div>
+  );
 }
 
 export async function loader({ params }) {
-    const order = getOrder(params.orderId);
-    return order;
+  const order = getOrder(params.orderId);
+  return order;
 }
 
 export default Order;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,32 +1,14 @@
 import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
-  const base = `
-        inline-block 
-        rounded-full 
-        bg-yellow-400 
-        font-semibold 
-        uppercase 
-        tracking-wide
-        text-stone-800 
-        transition-colors 
-        duration-300 
-        hover:bg-yellow-300
-        focus:bg-yellow-300
-        focus:outline-none
-        focus:ring
-        focus:ring-yellow-300
-        focus:ring-offset-2
-        active:bg-yellow-400
-        disabled:cursor-not-allowed
-        disabled:bg-slate-300
-    `;
+  const base =
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-yellow-400 disabled:cursor-not-allowed disabled:bg-slate-300";
 
   const styles = {
     primary: base + " md:px-6 md:py-4 px-4 py-3",
     small: base + " md:px-5 md:py-2.5 px-4 py-2 text-xs ",
     secondary:
-      "inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-200 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:bg-yellow-400 disabled:cursor-not-allowed disabled:bg-slate-300 md:px-6 md:py-3.5 px-4 py-2.5",
+      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-200 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:bg-yellow-400 disabled:cursor-not-allowed disabled:bg-slate-300 md:px-6 md:py-3.5 px-4 py-2.5",
   };
 
   if (to)
