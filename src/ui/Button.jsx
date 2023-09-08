08c0e57b1f375679478c4ce18b80689ab8705628
Glenn Pinto <gvpinto@gmail.com>
2023-09-08 12:54:12 -0400

    309. Styling the Cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,7 +5,6 @@ function Button({ children, disabled, to, type }) {
         inline-block 
         rounded-full 
         bg-yellow-400 
-
         font-semibold 
         uppercase 
         tracking-wide
@@ -24,8 +23,10 @@ function Button({ children, disabled, to, type }) {
     `;
 
   const styles = {
-    primary: base + " md:px-6 md:py-4 px-4 py-3 ",
+    primary: base + " md:px-6 md:py-4 px-4 py-3",
     small: base + " md:px-5 md:py-2.5 px-4 py-2 text-xs ",
+    secondary:
+      "inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-200 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 active:bg-yellow-400 disabled:cursor-not-allowed disabled:bg-slate-300 md:px-6 md:py-3.5 px-4 py-2.5",
   };
 
   if (to)
