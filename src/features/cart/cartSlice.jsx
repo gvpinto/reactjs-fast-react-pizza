adf34ff5f17f6eefbed00c24eff25b8c9f8116ba
Glenn Pinto <gvpinto@gmail.com>
2023-09-09 11:20:37 -0400

    315. Modelling the "Cart" State

---
+++ src/features/cart/cartSlice.jsx
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  //   cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      unitPrice: 12,
+      quantity: 2,
+      totalPrice: 24,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload == cart item
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload == pizzaId to be deleted
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // payload == pizzaId whose quantity needs to be increased
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+    },
+    decreaseItemQuantity(state, action) {
+      // payload == pizzaId whose quantity needs to be decreased
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+    },
+    clearCart(state, action) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
